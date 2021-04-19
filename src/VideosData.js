641d3f3930c94c440caeae1e81c0cb94e2f19be5
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-04-20 01:32:26 +0530

    added data

--- src/VideosData.js
+++ src/VideosData.js
@@ -110,14 +110,14 @@ function generateVideosData() {
       monthsAgo: faker.datatype.number(12)
     },
     {
-      id: "F8xQ5joLlD0",
-      thumbnail: "https://i3.ytimg.com/vi/F8xQ5joLlD0/hqdefault.jpg",
-      title: "Introduction to foundation",
-      videoLength: "10:07",
-      channel: "Pepcoding",
+      id: "scEDHsr3APg",
+      thumbnail: "https://i3.ytimg.com/vi/scEDHsr3APg/hqdefault.jpg",
+      title: "DevOps CI/CD Explained in 100 Seconds",
+      videoLength: "1:55",
+      channel: "Fireship",
       channelImg:
-        "https://yt3.ggpht.com/ytc/AAUvwnhIfHbN8Ucj_OEERe0uOsOHFJh9Giw_XFDP6dm_=s176-c-k-c0x00ffffff-no-rj",
-      link: "https://www.youtube.com/watch?v=F8xQ5joLlD0",
+        "https://yt3.ggpht.com/ytc/AAUvwnjWfh0Ki7uJWaYNhpZyG8ataxCL9N5SscOhfZfGXg=s88-c-k-c0x00ffffff-no-rj",
+      link: "https://www.youtube.com/watch?v=scEDHsr3APg",
       liked: false,
       disliked: false,
       views: faker.datatype.number(999),
@@ -127,29 +127,29 @@ function generateVideosData() {
 
   const Cloud = [
     {
-      id: "8hly31xKli0",
-      thumbnail: "https://i3.ytimg.com/vi/8hly31xKli0/hqdefault.jpg",
+      id: "usYySG1nbfI",
+      thumbnail: "https://i3.ytimg.com/vi/usYySG1nbfI/hqdefault.jpg",
       title:
-        "Algorithms and Data Structures - Full Course for Beginners from Treehouse",
-      videoLength: "5:22:00",
-      channel: "freeCodeCamp.org",
+        "Introduction to Cloud | Cloud Computing Tutorial for Beginners | Cloud Certifications | Edureka",
+      videoLength: "28:36",
+      channel: "edureka!",
       channelImg:
-        "https://yt3.ggpht.com/ytc/AAUvwnifaQZvAunS0OFb2y_cieoVjLCVjqQW8Exf3BC1gg=s68-c-k-c0x00ffffff-no-rj",
-      link: "https://www.youtube.com/watch?v=8hly31xKli0",
+        "https://yt3.ggpht.com/ytc/AAUvwni7i_dF9urC5uwdLIRQU89f5H4krFAgJgywql8kiA=s88-c-k-c0x00ffffff-no-rj",
+      link: "https://www.youtube.com/watch?v=usYySG1nbfI",
       liked: false,
       disliked: false,
       views: faker.datatype.number(999),
       monthsAgo: faker.datatype.number(12)
     },
     {
-      id: "92S4zgXN17o",
-      thumbnail: "https://i3.ytimg.com/vi/92S4zgXN17o/hqdefault.jpg",
-      title: "Introduction to data structures",
-      videoLength: "6:33",
-      channel: "mycodeschool",
+      id: "dH0yz-Osy54",
+      thumbnail: "https://i3.ytimg.com/vi/dH0yz-Osy54/hqdefault.jpg",
+      title: "What is Cloud Computing?",
+      videoLength: "3:11",
+      channel: "Amazon Web Services",
       channelImg:
-        "https://yt3.ggpht.com/ytc/AAUvwnjxmp9I5SL4FJUX1HWlg0k3jZVimZ6aNmuhPDJL=s88-c-k-c0x00ffffff-no-rj",
-      link: "https://www.youtube.com/watch?v=92S4zgXN17o",
+        "https://yt3.ggpht.com/ytc/AAUvwniWUd8_hJwiiwcIjwA8k6qQ7vvYhIJEtnNEnY0bb1U=s88-c-k-c0x00ffffff-no-rj",
+      link: "https://www.youtube.com/watch?v=dH0yz-Osy54",
       liked: false,
       disliked: false,
       views: faker.datatype.number(999),
